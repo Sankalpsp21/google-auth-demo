a200981353bd4e5f2eec90240be5d06f0184cb7d
Sankalp Patil <patilsa@oregonstate.edu>
2024-01-22 17:06:09 -0800

    final

--- app/page.js
+++ app/page.js
@@ -3,18 +3,19 @@
 import {
   SignedIn,
   SignedOut,
-  useAuth
+  useAuth,
+  useUser
 } from "@clerk/clerk-react";
 import { Sign } from "crypto";
 
 export default function Home() {
-  const user = useAuth();
+  const { user } = useUser();
 
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='text-center'>
         <SignedIn>
-          Welcome back!
+          Welcome back {user && `${user.firstName} ${user.lastName}`}!
         </SignedIn>
         <SignedOut>
           Welcome stranger!
